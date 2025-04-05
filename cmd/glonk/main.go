88911689fb60e9891446d95df4c5e541539be678
reshane <shanekenny023@gmail.com>
2025-04-05 02:01:50 -0400

    Add flag for storage options

--- cmd/glonk/main.go
+++ cmd/glonk/main.go
@@ -8,12 +8,19 @@ import (
     "github.com/reshane/glonk/store"
 )
 
+func getDb(which string) (store.Store, error) {
+	if which == "psql" {
+		return store.NewPsqlStore()
+	}
+	return store.NewSqliteStore()
+}
+
 func main() {
-    listenAddr := flag.String("listenaddr", ":8080", "The server address")
+	listenAddr := flag.String("listenaddr", ":8080", "The server address (default :8080)")
+	whichDb := flag.String("storage", "sqlite3", "The data storeage to use - psql: Postgres, sqlite3: Sqlite3 (default)")
     flag.Parse()
 
-    // db, err := store.NewPsqlStore()
-	db, err := store.NewSqliteStore()
+	db, err := getDb(*whichDb)
     if err != nil {
         log.Fatalf("Could not create db connection: %v", err)
     }
